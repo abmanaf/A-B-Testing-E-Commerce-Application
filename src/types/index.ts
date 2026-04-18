e3e47492f2e3e5e412499ae3f5b8db9067e169d6
abmanaf <abdulmanafaliu414@gmail.com>
2026-04-18 10:29:19 +0000

    style(config): standardize quote usage and add missing semicolons

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,17 @@
 export interface Category {
-  id: string
-  name: string
-  description: string | null
+  id: string;
+  name: string;
+  description: string | null;
 }
 
 export interface Product {
-  id: string
-  name: string
-  description: string | null
-  price: number
-  imageUrl: string
-  categoryId: string
-  category: Category
-  createdAt: string
-  updatedAt: string
+  id: string;
+  name: string;
+  description: string | null;
+  price: number;
+  imageUrl: string;
+  categoryId: string;
+  category: Category;
+  createdAt: string;
+  updatedAt: string;
 }
